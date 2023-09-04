3463a04e8c6d6e64bf20763a8d78b084fa38762a
surimsurim <skyrat3@gmail.com>
2023-09-04 22:01:46 +0900

    edittest

---
+++ edittest.py
@@ -0,0 +1,21 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+number_school = 2394584903
+str_name = "python"
+print(number_school)
+print(str_name)
+bool_view = False
+print(bool_view)
+flt_pi = 3.14
+print(flt_pi)
+char_element = 'a'
+print(char_element)
+chars = "hello world"
+print(chars)
+print("number : ", number_school, " name : ", str_name, " bool : ",bool_view, "float : ", flt_pi, "charcter : " , char_element, " charcters : ", chars)
