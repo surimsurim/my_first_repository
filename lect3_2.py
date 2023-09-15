39bde9e16d44004af9c01d65b611af36b5b75637
surimsurim <skyrat3@gmail.com>
2023-09-15 15:51:03 +0900

    first commit

---
+++ lect3_2.py
@@ -0,0 +1,41 @@
+"""x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+
+print(x is y)  
+print(x is z)  
+print(x is not y)  
+
+a = 5
+b = 5
+print(a is b)
+print(a is not b)
+
+a = 3
+b = 3.0
+print(a == b)
+print(a is b)
+print(a is not b)
+"""
+"""
+a = [3 , 5, 1]
+b = a
+print(a is b)
+
+a[0] = 2
+print(a, b)
+print(a is b)
+"""
+
+# a = 2 + 3 * 4
+# a = 10 / 5 / 2
+# a = 2 ** 3 ** 2
+#a = 10 % 3 % 2
+#a = 1 + 2 > 3 and 4 - 1 < 2
+#a = not False and True
+#a = not True or False and True
+#a = 1 * 2 | 3 ^ 4
+#a = 5 in [3, 4, 5] or 2 not in [1, 2, 3]
+#a = 2 * -3 // 2
+#a = 1 == 2
+a = 1==2 != 3
