de212d4183910776146b6601980fec81d787bdc5
surimsurim <skyrat3@gmail.com>
2023-09-18 16:19:12 +0900

    first commit

---
+++ lect4_1.py
@@ -0,0 +1,21 @@
+#input
+num = input("숫자를 입력하세요!!")
+print("number", int(num))
+
+#type
+a = 12
+print(type(a))
+
+a = 12.01
+print(type(a))
+
+a = "a"
+print(type(a))
+
+a = "abcd"
+print(type(a))
+
+a = [3,2,1]
+print(type(a))
+
+
