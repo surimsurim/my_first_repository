8318f8886d99ef155bc1d4d99798f1197f8cbc19
surimsurim <skyrat3@gmail.com>
2023-09-22 18:00:06 +0900

    first commit

---
+++ mod/str_util.py
@@ -0,0 +1,6 @@
+"""
+def cutstr(st. wd, idx):
+    tmp = st.split(wd)
+    res = tp[idx]
+    return res
+"""
---
+++ mod/utils.py
@@ -0,0 +1,34 @@
+"""
+import math
+import random
+
+def mt_sqrt(x):
+    return math.sqrt(x)
+
+ def mt_simpi():
+     return math.sin(math.pi /2)
+ 
+ def mt_elog():
+     return math.log(math.e)
+ 
+ def mt_exp(x):
+     return math.exp(x)
+ 
+ def mt_pi():
+     return math.pi
+"""
+"""
+def rd_int(x, y) :
+    return random.radiant(x, y)
+
+def rd_list(this):
+    return random.choice(this)
+
+def rd_rd():
+    return random.random()
+
+def rd_nmvar():
+    return random.normalvariate(0, 1)
+"""
+
+ 
